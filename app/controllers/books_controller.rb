e7c6b7b464a13ae4e44c8ff22eea588b111392fe
Hiroki1112Kurata <sequoia1112@yahoo.co.jp>
2021-06-12 05:55:46 +0000

    [Add]4-8_new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,25 @@
 class BooksController < ApplicationController
+  def new
+    @books = Book.new
+  end
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+    # 仮設定　一覧ページへリダイレクト
+  end
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
